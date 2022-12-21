7c9c2d72c389b9e602aab2820ae324184ffe75a1
Shibu Gope <shibugoper1@gmail.com>
2022-12-21 19:29:57 +0530

    first

--- src/main/java/com/example/BookMyShow/BookMyShowApplication.java
+++ src/main/java/com/example/BookMyShow/BookMyShowApplication.java
@@ -3,9 +3,11 @@ package com.example.BookMyShow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookMyShowApplication {
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@SpringBootApplication
+@EnableSwagger2
+public class BookMyShowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookMyShowApplication.class, args);
 	}
